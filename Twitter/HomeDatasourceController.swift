ad3b16a9553e48d831a2be6bedb13442a6a498b4
Ed <edmccormic@gmail.com>
2017-07-24 22:16:35 -0600

    Loaded Pod LBTAComponents

---
+++ Twitter/HomeDatasourceController.swift
@@ -0,0 +1,28 @@
+//
+//  HomeDatasourceController.swift
+//  Twitter
+//
+//  Created by Ed McCormic on 7/24/17.
+//  Copyright © 2017 Swiftbeard. All rights reserved.
+//
+
+import LBTAComponents
+
+
+class HomeDatasourceController: DatasourceController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let homeDatasource = HomeDatasource()
+        self.datasource = homeDatasource
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 50)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 100)
+    }
+    
+}
